9b9de702b6a2d68f69ee95dd820f45872df9b14f
brorica <minjea917@gmail.com>
2021-01-21 23:35:21 +0900

    DC

--- Algorithm/backTracking/2580.cpp
+++ Algorithm/backTracking/2580.cpp
@@ -6,14 +6,14 @@ using namespace std;
 #define square(i, j) (i / 3) * 3 + (j / 3)
 
 int board[9][9] = { 0, };
-int rowBoard[9][9] = { 0, };
-int columnBoard[9][9] = { 0, };
+int yBoard[9][9] = { 0, };
+int xBoard[9][9] = { 0, };
 int squareBoard[9][9] = { 0, };
 
 int func(int Count)
 {
 	int i, j;
-	int row = Count / 9, column = Count % 9;
+	int y = Count / 9, x = Count % 9;
 	if (Count == 81)
 	{
 		for (i = 0; i < 9; i++)
@@ -24,21 +24,23 @@ int func(int Count)
 		}
 		exit(0);
 	}
-	if (board[row][column] == 0)
+	if (board[y][x] == 0)
 	{
 		for (i = 1; i <= 9; i++)
 		{
-			if (rowBoard[row][i] == 0 && columnBoard[column][i] == 0 && squareBoard[square(row, column)][i] == 0)
+			if (yBoard[y][i] == 0 && xBoard[x][i] == 0 && squareBoard[square(y, x)][i] == 0)
 			{
-				board[row][column] = i;
-				rowBoard[row][i] = 1;
-				columnBoard[column][i] = 1;
-				squareBoard[square(row, column)][i] = 1;
+				// 일단 빈 숫자가 있으면 넣어보고,
+				board[y][x] = i;
+				yBoard[y][i] = 1;
+				xBoard[x][i] = 1;
+				squareBoard[square(y, x)][i] = 1;
 				func(Count + 1);
-				board[row][column] = 0;
-				rowBoard[row][i] = 0;
-				columnBoard[column][i] = 0;
-				squareBoard[square(row, column)][i] = 0;
+				// 아니면 다시 뺀다.
+				board[y][x] = 0;
+				yBoard[y][i] = 0;
+				xBoard[x][i] = 0;
+				squareBoard[square(y, x)][i] = 0;
 			}
 		}
 	}
@@ -59,8 +61,8 @@ int main()
 			cin >> board[i][j];
 			if (board[i][j] != 0)
 			{
-				rowBoard[i][board[i][j]] = 1;
-				columnBoard[j][board[i][j]] = 1;
+				yBoard[i][board[i][j]] = 1;
+				xBoard[j][board[i][j]] = 1;
 				squareBoard[square(i, j)][board[i][j]] = 1;
 			}
 		}
